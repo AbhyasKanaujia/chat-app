b9c6c5607dace725efad81d890d65d8acc0c9065
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2024-11-10 20:26:03 +0530

    Add socket.io

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const path = require("path");
-
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.listen(process.env.PORT || 5001, () =>
+io.on("connection", () => {
+  console.log("New websocket connection");
+});
+
+server.listen(process.env.PORT || 5001, () =>
   console.log(`Server running on port ${process.env.PORT || 5001}`)
 );
