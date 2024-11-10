aee1374fc62f31f5158e21f90ad2f5ee5b2219fc
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2024-11-10 17:25:07 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.listen(process.env.PORT || 5001, () =>
+  console.log(`Server running on port ${process.env.PORT || 5001}`)
+);
