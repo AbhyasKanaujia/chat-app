f4391d950550f5addd36c8d147596325b42f4446
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2024-11-13 20:39:52 +0530

    Add event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,7 +16,13 @@ socket.on("message", (message) => {
 document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("The message was delivered");
+  });
 });
 
 document.querySelector("#share-location").addEventListener("click", () => {
@@ -25,9 +31,15 @@ document.querySelector("#share-location").addEventListener("click", () => {
   }
 
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location shared");
+      }
+    );
   });
 });
