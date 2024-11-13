c28739c9415038ae7619aad124e5c32e73250100
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2024-11-13 20:53:23 +0530

    Disable form buttons while processing and refocus input fields

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,39 @@
 const socket = io();
 
-// socket.on("countUpdated", (count) => {
-//   console.log(`The count has been updated to ${count}`);
-// });
-
-// document.querySelector("#increment").addEventListener("click", () => {
-//   socket.emit("increment");
-// });
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $shareLocationButton = document.querySelector("#share-location");
 
 const message = [];
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
-
     console.log("The message was delivered");
   });
 });
 
-document.querySelector("#share-location").addEventListener("click", () => {
+$shareLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+
+  $shareLocationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
@@ -38,6 +43,7 @@ document.querySelector("#share-location").addEventListener("click", () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $shareLocationButton.removeAttribute("disabled");
         console.log("Location shared");
       }
     );
